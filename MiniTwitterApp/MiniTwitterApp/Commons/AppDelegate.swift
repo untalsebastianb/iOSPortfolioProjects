3c7905bb30cee2f04aac8165141a4dd3fcfe0756
Sebastian <>
2022-03-12 13:46:46 -0500

    Create Welcome view

---
+++ MiniTwitterApp/MiniTwitterApp/Commons/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  MiniTwitterApp
+//
+//  Created by Sebastian on 12/03/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
