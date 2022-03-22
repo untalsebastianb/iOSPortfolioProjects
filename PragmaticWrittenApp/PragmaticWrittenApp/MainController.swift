4086e1574e1a3003b791a6c55aad61d21c5e3a34
Sebastian <>
2022-03-22 06:40:07 -0500

    Config the project to work without storyboards

---
+++ PragmaticWrittenApp/PragmaticWrittenApp/MainController.swift
@@ -0,0 +1,19 @@
+//
+//  Main.swift
+//  PragmaticWrittenApp
+//
+//  Created by Sebastian on 21/03/22.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
