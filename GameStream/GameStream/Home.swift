e21f993cc03a72789f3b5f7a1107099f371348cb
Sebastian <>
2022-04-10 20:35:00 -0500

    Create suggested and recommended categories along with the scroll view

--- GameStream/GameStream/Home.swift
+++ GameStream/GameStream/Home.swift
@@ -151,6 +151,99 @@ struct HomeScreenSubModule: View {
             }.frame( maxWidth: .infinity, alignment: .center)
                 .padding(.vertical)
             
+            
+            Text("SUGGESTED CATEGORIES")
+                .font(.title3)
+                .foregroundColor(.white)
+                .bold()
+                .frame( maxWidth: .infinity, alignment: .leading)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    
+                    Button(action: {}) {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(Color("Blue-Gray"))
+                                .frame(width: 160, height: 90)
+                            
+                            Image("FPS")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 42, height: 42)
+                        }
+                    }
+                    Button(action: {}) {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(Color("Blue-Gray"))
+                                .frame(width: 160, height: 90)
+                            
+                            Image("RPG")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 42, height: 42)
+                        }
+                    }
+                    Button(action: {}) {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(Color("Blue-Gray"))
+                                .frame(width: 160, height: 90)
+                            
+                            Image("OpenWorld")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 42, height: 42)
+                        }
+                    }
+                    
+                }
+            }
+            
+            Text("RECOMMENDED")
+                .font(.title3)
+                .foregroundColor(.white)
+                .bold()
+                .frame( maxWidth: .infinity, alignment: .leading)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack{
+                    Button(action: {
+                        url = urlVideos[1]
+                        print("URL: \(url)")
+                        isPlayerActive = true
+                    }) {
+                        Image("Abzu")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 240, height: 135)
+                    }
+                    
+                    Button(action: {
+                        url = urlVideos[2]
+                        print("URL: \(url)")
+                        isPlayerActive = true
+                    }) {
+                        Image("Crash Bandicoot")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 240, height: 135)
+                    }
+                    
+                    Button(action: {
+                        url = urlVideos[3]
+                        print("URL: \(url)")
+                        isPlayerActive = true
+                    }) {
+                        Image("DEATH STRANDING")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 240, height: 135)
+                    }
+                }
+            }
+            
         }
         
         
