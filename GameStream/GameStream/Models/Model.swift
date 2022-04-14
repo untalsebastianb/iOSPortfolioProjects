bef694903913c859e113b51bec3976d88f56450c
Sebastian <>
2022-04-14 10:48:24 -0500

    Model the json response

--- GameStream/GameStream/Models/Model.swift
+++ GameStream/GameStream/Models/Model.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+struct Games: Codable {
+    var games:[Game]
+}
+
+struct Game: Codable {
+    var title: String
+    var studio: String
+    var contentRating: String
+    var publicationYear: String
+    var description: String
+    var platforms: [String]
+    var tags: [String]
+    var videosURL: VideoURL
+    var galleryImages: [String]
+}
+
+struct VideoURL: Codable {
+    var mobile: String
+    var tablet: String
+}
