44086ebdbc14f1703b1cfd056cfea0c2262c6039
Sebastian <>
2022-04-17 14:47:02 -0500

    Create the gamesView with LazyVGrid

--- GameStream/GameStream/Views/GamesView.swift
+++ GameStream/GameStream/Views/GamesView.swift
@@ -6,14 +6,73 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct GamesView: View {
     
     @ObservedObject var games = ViewModel()
+    @State var gameViewIsActive: Bool   = false
+    @State var url: String              = ""
+    @State var titulo: String           = ""
+    @State var studio: String           = ""
+    @State var calification: String     = ""
+    @State var publicationYear: String  = ""
+    @State var description: String      = ""
+    @State var tags: [String]           = []
+    @State var imgsUrl: [String]        = []
+    
+    let gridLayout = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
     
     var body: some View {
-        Text("Hello, fron games view!")
-            .onAppear {
+        ZStack {
+            
+            Color("Marine-Color").ignoresSafeArea()
+            
+            VStack{
+                
+                Text("Games")
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding(EdgeInsets(top: 16, leading: 0, bottom: 64, trailing: 0))
+                
+                ScrollView{
+                    LazyVGrid(columns: gridLayout, spacing: 8) {
+                        
+                        ForEach(games.gamesInfo, id: \.self) {
+                            game in
+                            
+                            Button {
+                                url = game.videosUrls.mobile
+                                titulo = game.title
+                                calification = game.contentRaiting
+                                publicationYear = game.publicationYear
+                                description = game.description
+                                tags = game.tags
+                                imgsUrl = game.galleryImages
+                                
+                                print("pulse el juego \(titulo)")
+                                
+                            } label: {
+                                Text("\(game.title)")
+                                
+                                KFImage(<#T##url: URL?##URL?#>)
+                            }
+
+                        }
+                        
+                    }
+                }
+                
+            }.padding(.horizontal)
+            
+            
+            
+            
+        }.onAppear {
                 print("First element of json: \(games.gamesInfo[0])")
                 print("The view is showing \(games.gamesInfo[0].title)")
             }
