df79802870e3b2007a59cf1ec33f7950197075e2
Sebastian <>
2022-04-19 11:41:02 -0500

    Build game gallery and comments card

--- GameStream/GameStream/Views/GameView.swift
+++ GameStream/GameStream/Views/GameView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import AVKit
+import Kingfisher
 
 struct GameDTO {
     var url: String
@@ -81,6 +82,103 @@ struct GameInfoView: View {
     }
 }
 
+struct GameGallery: View {
+    
+    var imgsUrl: [String]
+    let layout = [
+        GridItem(.flexible())
+    ]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Gallery")
+                .foregroundColor(.white)
+                .font(.title3)
+                .padding(.leading)
+            
+            ScrollView(.horizontal){
+                LazyHGrid(rows: layout, spacing: 8){
+                    
+                    ForEach(imgsUrl, id: \.self) {
+                        url in
+                        KFImage(URL(string: url))
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                    }
+                    
+                }
+            }.frame(height: 180)
+            
+        }.frame(maxWidth: .infinity, alignment: .leading)
+        
+    }
+}
+
+struct CommentCard: View {
+    
+    var data: [GameCommentInfo]
+    
+    var body: some View {
+        
+        ForEach(data, id: \.self) {
+            comment in
+            ZStack(alignment: .leading){
+                    RoundedRectangle(cornerRadius: 8.0)
+                        .fill(Color("TabBar-Color"))
+                    VStack(alignment: .leading){
+                        HStack(alignment: .top){
+                            Image(comment.profileImg)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 45, height: 45)
+                                .clipShape(Circle())
+                            
+                            VStack(alignment: .leading, spacing: 5) {
+                                Text(comment.user)
+                                    .font(.body)
+                                    .foregroundColor(.white)
+                                Text(comment.time)
+                                    .font(.footnote)
+                                    .foregroundColor(.white)
+                            }
+                        }.padding(.bottom)
+                        
+                        Text(comment.commentText)
+                            .lineLimit(nil)
+                            .foregroundColor(.white)
+                            .font(.body)
+                        
+                        
+                    }.padding(EdgeInsets(top: 10, leading: 15, bottom: 10, trailing: 5))
+                }.padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
+        }
+    }
+}
+
+struct GameCommentInfo: Hashable {
+    var profileImg: String
+    var user: String
+    var time: String
+    var commentText: String
+}
+
+struct GameCommentsView: View {
+    
+    var gameComments: [GameCommentInfo]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Comments")
+                .foregroundColor(.white)
+                .font(.title3)
+                .padding(.leading)
+            
+            CommentCard(data: gameComments)
+        }
+            
+    }
+}
+
 struct GameView: View {
     var game: GameDTO?
     var body: some View {
@@ -99,6 +197,9 @@ struct GameView: View {
                                                     tags: game.tags,
                                                     imgsUrl: [""]))
                             .padding(.bottom)
+                        
+                        GameGallery(imgsUrl: game.imgsUrl)
+                        GameCommentsView(gameComments: [GameCommentInfo(profileImg: "Abzu", user: "Sebastian", time: "1 month ago", commentText: "The game is truly good"), GameCommentInfo(profileImg: "Hades", user: "Camilo", time: "6 months ago", commentText: "GREAT GAME")])
                     }
                     
                 }.frame(maxWidth: .infinity)
