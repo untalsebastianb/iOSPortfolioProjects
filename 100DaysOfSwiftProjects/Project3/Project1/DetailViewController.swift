f01360916cf1fe39622817171959b08b4ecafaff
Sebastian <>
2022-04-25 14:32:21 -0500

    Add image name to selected items

--- 100DaysOfSwiftProjects/Project3/Project1/DetailViewController.swift
+++ 100DaysOfSwiftProjects/Project3/Project1/DetailViewController.swift
@@ -28,7 +28,7 @@ class DetailViewController: UIViewController {
             return
         }
         
-        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        let vc = UIActivityViewController(activityItems: [image, selectedImage ?? "default"], applicationActivities: [])
         vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
         present(vc, animated: true)
     }
