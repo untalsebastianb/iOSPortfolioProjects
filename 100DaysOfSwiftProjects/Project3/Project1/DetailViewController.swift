d7e771f6f058f1accd11e2356d4d44eae4f6d3ba
Sebastian <>
2022-04-25 11:39:38 -0500

    Create the navigationItem rightBarButton to share

---
+++ 100DaysOfSwiftProjects/Project3/Project1/DetailViewController.swift
@@ -0,0 +1,47 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Sebastian on 15/04/22.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        print("la vista aparecio")
+        super.viewDidLoad()
+        navigationItem.largeTitleDisplayMode = .never
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+
+
+}
