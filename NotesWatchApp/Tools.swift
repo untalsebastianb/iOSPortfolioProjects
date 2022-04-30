3b4c6dcd9f1a4a4d3a73ef659fa62a4480496541
Sebastian <>
2022-04-29 22:00:19 -0500

    Create Tools file for saving and loading data using singleton

---
+++ NotesWatchApp/Tools.swift
@@ -0,0 +1,31 @@
+//
+//  Tools.swift
+//  NotesWatchApp WatchKit Extension
+//
+//  Created by Sebastian on 29/04/22.
+//
+
+import Foundation
+
+class Tools {
+    let key = "NotesApp"
+    
+    static let shared = Tools()
+    
+    private init(){ }
+    
+    func save(notes: [Note]) {
+        print("Data sin encodear", notes)
+        
+        let data = notes.map{ try? JSONEncoder().encode($0) }
+        
+        print("Data encodeada", data)
+        UserDefaults.standard.set(data, forKey: key)
+    }
+    
+    func load() -> [Note] {
+        guard let savedData = UserDefaults.standard.array(forKey: key) as? [Data] else { return [] }
+        return savedData.map{ try! JSONDecoder().decode(Note.self, from: $0)}
+    }
+
+}
