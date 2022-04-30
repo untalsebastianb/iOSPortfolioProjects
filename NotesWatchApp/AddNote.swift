ee5ee50d62556c43f33fc4b7ad98c821511d6d93
Sebastian <>
2022-04-29 21:11:27 -0500

    Create addNote view

--- NotesWatchApp/AddNote.swift
+++ NotesWatchApp/AddNote.swift
@@ -13,15 +13,20 @@ struct AddNote: View {
     @State private var text = ""
     
     var body: some View {
-        TextField("Note", text: $text)
-        Button("Add note"){
-            guard text.isEmpty == false else {
-                return
+        VStack {
+            TextField("Note", text: $text)
+            Button("Add note"){
+                guard text.isEmpty == false else {
+                    return
+                }
+                let note = Note(title: text)
+                notes.append(note)
+                text = ""
+                
+                print("NOTESS:", notes)
             }
-            let note = Note(title: text, creationDate: "22-02-2022")
-            notes.append(note)
-            text = ""
         }
+        
     }
 }
 
