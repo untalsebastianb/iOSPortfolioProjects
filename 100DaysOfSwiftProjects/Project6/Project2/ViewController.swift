450e40025f8aba513c09b8ec7b1d6e1049a5ee02
Sebastian <>
2022-05-11 15:33:10 -0500

    Setting up project 6

---
+++ 100DaysOfSwiftProjects/Project6/Project2/ViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Sebastian on 18/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
+    var timesButtonsTapped = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem.init(image: UIImage(systemName: "info.circle"), style: .done, target: self, action: #selector(getScore))
+
+        askQuestion()
+    }
+    
+    @objc func getScore() {
+        let ac = UIAlertController(title: "Info", message: "Your score is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: nil))
+        present(ac, animated: true)
+        
+    }
+
+    func askQuestion(action: UIAlertAction! = nil){
+        
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
+        title! += " - Score: \(score)"
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "You're wrong that's the flag of: \(countries[sender.tag]) "
+            score -= 1
+        }
+        timesButtonsTapped += 1
+                
+        if timesButtonsTapped == 10 {
+            title = "You have reach your 10 attemps"
+        }
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
+        
+    }
+    
+
+}
+
---
+++ 100DaysOfSwiftProjects/Project6/README.md
@@ -0,0 +1,22 @@
+
+On the days 19th, 20th, 21st of #100DaysOfSwift challenge, I developed this funny game about guessing the flag<br>
+once you start tapping on the flag you'll notify with an alet and automatically raise or decrease your score<br>
+
+I went through: <br>
+‣ Array shuffling <br>
+‣ Random number generation <br>
+‣ IBAction <br>
+‣ UIAlertController <br>
+‣ AutoLayout <br>
+‣ View controllers, storyboards, UIImage <br>
+
+Demo: <br>
+![Project evidence](https://github.com/untalsebastianb/iOSPortfolioProjects/blob/main/100DaysOfSwiftProjects/Project2/Evidence3.gif)
+
+On day 22nd, I added the rightBarButtonItem to show the user score. <br>
+
+Demo: <br>
+![Project evidence](https://github.com/untalsebastianb/iOSPortfolioProjects/blob/main/100DaysOfSwiftProjects/Project2/InfoScoreButtonGif.gif)
+
+
+Thanks for your time 🕰
