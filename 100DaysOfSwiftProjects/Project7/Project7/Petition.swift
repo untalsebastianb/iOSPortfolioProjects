3a96b5cd83192e5437f9cdcded08a959cd44950c
Sebastian <>
2022-05-22 00:01:13 -0500

    Create the codable models to parse the JSON

---
+++ 100DaysOfSwiftProjects/Project7/Project7/Petition.swift
@@ -0,0 +1,16 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Sebastian on 21/05/22.
+//
+
+import Foundation
+
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
+
---
+++ 100DaysOfSwiftProjects/Project7/Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Sebastian on 21/05/22.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
