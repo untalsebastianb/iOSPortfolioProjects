b4732e6b1bcb3dd098f8fd2e70a020e625de9b33
Sebastian <>
2022-05-22 00:01:38 -0500

    Hanldle the results on the UI and updating the view

--- 100DaysOfSwiftProjects/Project7/Project7/ViewController.swift
+++ 100DaysOfSwiftProjects/Project7/Project7/ViewController.swift
@@ -7,11 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var petitions = [Petition]()
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                parse(json: data)
+                return
+            }
+        }
+    }
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            print("⚠️", jsonPetitions.results)
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+        }
+    }
+    
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let petition = petitions[indexPath.row]
+        print("PETITIONS:" , petitions[indexPath.row])
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
+        
+        return cell
     }
 
 
