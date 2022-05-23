7258aa0b58c0cf89998005c7aa4af7b495f79176
Sebastian <>
2022-05-22 22:36:51 -0500

    Identify the tabBarItem

--- 100DaysOfSwiftProjects/Project7/Project7/ViewController.swift
+++ 100DaysOfSwiftProjects/Project7/Project7/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 class ViewController: UITableViewController {
     
     var petitions = [Petition]()
-
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        let urlString: String
+        
+        if  navigationController?.tabBarItem.tag == 0 {
+            urlString =  "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString =  "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
         
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
@@ -22,34 +28,43 @@ class ViewController: UITableViewController {
                 return
             }
         }
+        showError()
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed, please check your connection and try again", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Okay", style: .default))
+        present(ac, animated: true)
     }
     
     func parse(json: Data) {
         let decoder = JSONDecoder()
         
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
-            print("⚠️", jsonPetitions.results)
             petitions = jsonPetitions.results
             tableView.reloadData()
         }
     }
     
-    
-    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         let petition = petitions[indexPath.row]
-        print("PETITIONS:" , petitions[indexPath.row])
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         
         return cell
     }
-
-
+    
+    
 }
 
