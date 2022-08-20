c6b9478aaaca36d7d7af5b6dfe03ce33e15d7dc0
Sebastian <>
2022-08-20 10:20:50 -0500

    Indent code

--- Concepts/iOS_Networking_RayWenderlich/HalfTunes_Pause_Cancel_ResumeTask/HalfTunes/Model/SongDownload.swift
+++ Concepts/iOS_Networking_RayWenderlich/HalfTunes_Pause_Cancel_ResumeTask/HalfTunes/Model/SongDownload.swift
@@ -33,73 +33,73 @@
 import SwiftUI
 
 class SongDownload: NSObject, ObservableObject {
-  
-  var downloadTask: URLSessionDownloadTask?
-  var downloadURL: URL?
-  
-  @Published var downloadLocation: URL?
-  @Published var downloadedAmount: Float = 0
-  @Published var isDownloading = false
-  
-  lazy var urlSession: URLSession = {
-    let configuration = URLSessionConfiguration.default
-    return URLSession(configuration: configuration, delegate: self, delegateQueue: nil)
-  }()
-  
-  func fetchSongAtUrl(_ item: URL) {
-    downloadURL = item
-    downloadTask = urlSession.downloadTask(with: item)
-    downloadTask?.resume()
-    isDownloading = true
-  }
-  
-  
+    
+    var downloadTask: URLSessionDownloadTask?
+    var downloadURL: URL?
+    
+    @Published var downloadLocation: URL?
+    @Published var downloadedAmount: Float = 0
+    @Published var isDownloading = false
+    
+    lazy var urlSession: URLSession = {
+        let configuration = URLSessionConfiguration.default
+        return URLSession(configuration: configuration, delegate: self, delegateQueue: nil)
+    }()
+    
+    func fetchSongAtUrl(_ item: URL) {
+        downloadURL = item
+        downloadTask = urlSession.downloadTask(with: item)
+        downloadTask?.resume()
+        isDownloading = true
+    }
+    
+    
 }
 
 extension SongDownload: URLSessionDownloadDelegate {
-  
-  func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
     
-    DispatchQueue.main.async {
-      self.downloadedAmount = Float(totalBytesWritten) / Float(totalBytesExpectedToWrite)
-      print(self.downloadedAmount)
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+        
+        DispatchQueue.main.async {
+            self.downloadedAmount = Float(totalBytesWritten) / Float(totalBytesExpectedToWrite)
+            print(self.downloadedAmount)
+        }
+        
     }
     
-  }
-  
-  func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
-    if let error = error {
-      print(error.localizedDescription)
-    }
-    DispatchQueue.main.async {
-      self.isDownloading = false
-    }
-    print("finished")
-  }
-  
-  
-  func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
-    
-    let fileManager = FileManager.default
-    guard let documentsPath = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first,
-      let lastPathComponent = downloadURL?.lastPathComponent else {
-        fatalError()
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        if let error = error {
+            print(error.localizedDescription)
+        }
+        DispatchQueue.main.async {
+            self.isDownloading = false
+        }
+        print("finished")
     }
     
-    let destinationUrl = documentsPath.appendingPathComponent(lastPathComponent)
-    do {
-      if fileManager.fileExists(atPath: destinationUrl.path) {
-        try fileManager.removeItem(at: destinationUrl)
-      }
-      try fileManager.copyItem(at: location, to: destinationUrl)
-      DispatchQueue.main.async {
-        self.downloadLocation = destinationUrl
-      }
-    } catch {
-      print(error)
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        
+        let fileManager = FileManager.default
+        guard let documentsPath = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first,
+              let lastPathComponent = downloadURL?.lastPathComponent else {
+            fatalError()
+        }
+        
+        let destinationUrl = documentsPath.appendingPathComponent(lastPathComponent)
+        do {
+            if fileManager.fileExists(atPath: destinationUrl.path) {
+                try fileManager.removeItem(at: destinationUrl)
+            }
+            try fileManager.copyItem(at: location, to: destinationUrl)
+            DispatchQueue.main.async {
+                self.downloadLocation = destinationUrl
+            }
+        } catch {
+            print(error)
+        }
+        
     }
     
-  }
-  
-  
+    
 }
--- Concepts/iOS_Networking_RayWenderlich/HalfTunes_Pause_Cancel_ResumeTask/HalfTunes/Views/SongDetailView.swift
+++ Concepts/iOS_Networking_RayWenderlich/HalfTunes_Pause_Cancel_ResumeTask/HalfTunes/Views/SongDetailView.swift
@@ -33,87 +33,87 @@
 import SwiftUI
 
 struct SongDetailView: View {
-
-  @Binding var musicItem: MusicItem
-  @State private var playMusic = false
-  @ObservedObject var download = SongDownload()
-  @State private var musicImage = UIImage(named: "c_urlsession_card_artwork")!
-  
-  var body: some View {
-    VStack {
-      GeometryReader { reader in
+    
+    @Binding var musicItem: MusicItem
+    @State private var playMusic = false
+    @ObservedObject var download = SongDownload()
+    @State private var musicImage = UIImage(named: "c_urlsession_card_artwork")!
+    
+    var body: some View {
         VStack {
-          Image(uiImage: self.musicImage)
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .frame(height: reader.size.height / 2)
-            .cornerRadius(50)
-            .padding()
-            .shadow(radius: 10)
-          Text("\(self.musicItem.trackName) - \(self.musicItem.artistName)")
-          Text(self.musicItem.collectionName)
-          if (self.download.isDownloading) {
-            Text("\(Int(self.download.downloadedAmount * 100))% downloaded")
-              .padding(.top)
-          }
-          Button(action: self.downloadButtonTapped) {
-            Text(self.download.downloadLocation == nil ? "Download" : "Listen")
-          }.sheet(isPresented: self.$playMusic) {
-            return AudioPlayer(songUrl: self.download.downloadLocation!)
-          }
+            GeometryReader { reader in
+                VStack {
+                    Image(uiImage: self.musicImage)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: reader.size.height / 2)
+                        .cornerRadius(50)
+                        .padding()
+                        .shadow(radius: 10)
+                    Text("\(self.musicItem.trackName) - \(self.musicItem.artistName)")
+                    Text(self.musicItem.collectionName)
+                    if (self.download.isDownloading) {
+                        Text("\(Int(self.download.downloadedAmount * 100))% downloaded")
+                            .padding(.top)
+                    }
+                    Button(action: self.downloadButtonTapped) {
+                        Text(self.download.downloadLocation == nil ? "Download" : "Listen")
+                    }.sheet(isPresented: self.$playMusic) {
+                        return AudioPlayer(songUrl: self.download.downloadLocation!)
+                    }
+                }
+            }
+        }.onAppear(perform: displayAlbumArt)
+    }
+    
+    func displayAlbumArt() {
+        guard let albumImageUrl = URL(string: musicItem.artwork) else {
+            return
         }
-      }
-    }.onAppear(perform: displayAlbumArt)
-  }
-  
-  func displayAlbumArt() {
-    guard let albumImageUrl = URL(string: musicItem.artwork) else {
-      return
+        let task = URLSession.shared.downloadTask(with: albumImageUrl) { location, response, error in
+            
+            guard let location = location,
+                  let imageData = try? Data(contentsOf: location),
+                  let image = UIImage(data: imageData) else {
+                return
+            }
+            DispatchQueue.main.async {
+                self.musicImage = image
+            }
+        }
+        task.resume()
+        
+        
     }
-    let task = URLSession.shared.downloadTask(with: albumImageUrl) { location, response, error in
-      
-      guard let location = location,
-            let imageData = try? Data(contentsOf: location),
-        let image = UIImage(data: imageData) else {
-          return
-      }
-      DispatchQueue.main.async {
-        self.musicImage = image
-      }
+    
+    func downloadButtonTapped() {
+        if self.download.downloadLocation == nil {
+            guard let previewUrl = self.musicItem.previewUrl else {
+                return
+            }
+            self.download.fetchSongAtUrl(previewUrl)
+        } else {
+            self.playMusic = true
+        }
     }
-    task.resume()
     
     
-  }
-  
-  func downloadButtonTapped() {
-    if self.download.downloadLocation == nil {
-      guard let previewUrl = self.musicItem.previewUrl else {
-        return
-      }
-      self.download.fetchSongAtUrl(previewUrl)
-    } else {
-      self.playMusic = true
-    }
-  }
-  
-  
 }
 
 
 struct SongDetailView_Previews: PreviewProvider {
-
-  
-  struct PreviewWrapper: View {
-    @State private var musicItem = MusicItem(id: 192678693, artistName: "Leonard Cohen", trackName: "Hallelujah", collectionName: "The Essential Leonard Cohen", preview: "https://audio-ssl.itunes.apple.com/itunes-assets/Music/16/10/b2/mzm.muynlhgk.aac.p.m4a", artwork: "https://is1-ssl.mzstatic.com/image/thumb/Music/v4/77/17/ab/7717ab31-46f9-48ca-7250-9f565306faa7/source/1000x1000bb.jpg")
     
-    var body: some View {
-      SongDetailView(musicItem: $musicItem)
+    
+    struct PreviewWrapper: View {
+        @State private var musicItem = MusicItem(id: 192678693, artistName: "Leonard Cohen", trackName: "Hallelujah", collectionName: "The Essential Leonard Cohen", preview: "https://audio-ssl.itunes.apple.com/itunes-assets/Music/16/10/b2/mzm.muynlhgk.aac.p.m4a", artwork: "https://is1-ssl.mzstatic.com/image/thumb/Music/v4/77/17/ab/7717ab31-46f9-48ca-7250-9f565306faa7/source/1000x1000bb.jpg")
+        
+        var body: some View {
+            SongDetailView(musicItem: $musicItem)
+        }
     }
-  }
-  
-  static var previews: some View {
-    PreviewWrapper()
-  }
+    
+    static var previews: some View {
+        PreviewWrapper()
+    }
 }
 
