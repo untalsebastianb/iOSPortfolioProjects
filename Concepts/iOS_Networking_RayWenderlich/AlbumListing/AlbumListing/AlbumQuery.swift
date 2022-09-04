0c4a635afb6edc06fb46e57a194c8233a5e5948e
Sebastian <>
2022-09-04 09:54:35 -0500

    Create the data Task publisher, reveice on the main thread and sink to view values

--- Concepts/iOS_Networking_RayWenderlich/AlbumListing/AlbumListing/AlbumQuery.swift
+++ Concepts/iOS_Networking_RayWenderlich/AlbumListing/AlbumListing/AlbumQuery.swift
@@ -45,6 +45,15 @@ final class AlbumQuery: ObservableObject {
     
     init() {
         let albumsURL = URL(string: "https://api.npoint.io/e502540aa6515916620e")!
-        
+        URLSession.shared.dataTaskPublisher(for: albumsURL)
+            .map { $0.data }
+            .decode(type: Albums.self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                print(completion)
+            } receiveValue: { album in
+                self.titles = album.titles
+            }
+            .store(in: &cancellables)
     }
 }
