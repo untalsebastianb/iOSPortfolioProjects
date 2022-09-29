e84b6e8665dc14f6eaf7a5fd67d6f288ff9baed9
Sebastian <>
2022-09-29 12:56:32 -0500

    set up project and colors scheme

---
+++ Instagram-SwiftUI/Instagram-SwiftUI/Instagram_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_SwiftUIApp.swift
+//  Instagram-SwiftUI
+//
+//  Created by Sebastian on 29/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
