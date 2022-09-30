426ace5dfff98c331629a0a3c51b7405cfd6c0f0
Sebastian <>
2022-09-30 15:53:23 -0500

    Set main view and use story view

--- Instagram-SwiftUI/Instagram-SwiftUI/ContentView.swift
+++ Instagram-SwiftUI/Instagram-SwiftUI/ContentView.swift
@@ -8,16 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject private var viewModel = ViewModel()
+    
     var body: some View {
         NavigationStack {
-            ScrollView {
-                VStack {
-                    Text("Hola")
-                }
-                .toolbar {
-                    self.toolbarView()
+            VStack(spacing:0) {
+                Divider().overlay(Color("primary")).frame(height: 1).opacity(0.4)
+                ScrollView {
+                    VStack {
+                        storyList
+                    }
+                    .toolbar {
+                        self.toolbarView()
+                    }
                 }
             }
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+    
+    var storyList: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(viewModel.storyList) { story in
+                    StoryView(story: story)
+                }
+            }
+            .padding(.leading, 10)
+            .padding(.vertical,8)
         }
     }
     
