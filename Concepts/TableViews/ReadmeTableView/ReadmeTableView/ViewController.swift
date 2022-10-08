ac8e28a8c51a12e2b84f90bc71e1f0e5a381efa5
Sebastian <>
2022-10-08 09:31:51 -0500

    Setting up the table view

---
+++ Concepts/TableViews/ReadmeTableView/ReadmeTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReadmeTableView
+//
+//  Created by Sebastian on 8/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Instagram-SwiftUI/Instagram-SwiftUI/Views/TicketView.swift
@@ -0,0 +1,20 @@
+//
+//  TicketView.swift
+//  MovieBooking
+//
+//  Created by Sebastian on 5/10/22.
+//
+
+import SwiftUI
+
+struct TicketView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TicketView_Previews: PreviewProvider {
+    static var previews: some View {
+        TicketView()
+    }
+}
