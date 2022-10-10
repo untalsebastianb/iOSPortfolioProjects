fa9a878f1fe7f8cd4ad7b04939ee54af7eeeafb9
Sebastian <>
2022-10-10 14:55:33 -0500

    Create a basic call with no generics

--- Concepts/PassingDataBetweenVC's/Generics/Generics/ViewController.swift
+++ Concepts/PassingDataBetweenVC's/Generics/Generics/ViewController.swift
@@ -17,3 +17,43 @@ class ViewController: UIViewController {
 
 }
 
+class APICaller {
+    static let shared = APICaller()
+    
+    public func getFruits() {
+        URLSession.shared.dataTask(with: URL(string: "foo")!) { data, response, error in
+            guard let data = data , error == nil else {
+                return
+            }
+            
+//            if this is a car you need to replace the type, is not scallable
+            let fruit: Fruit?
+            do {
+                fruit = try JSONDecoder().decode(Fruit.self, from: data)
+            }
+            catch {
+                print(error)
+            }
+        }
+    }
+}
+
+// you probably will have more than one API CALL
+
+struct Fruit: Codable {
+    let name: String
+    let identifier: String
+    let imageName: String
+}
+
+struct Car: Codable {
+    let name: String
+    let identifier: String
+    let imageName: String
+}
+
+struct Recipes: Codable {
+    let name: String
+    let recipiesType: String
+    let imageName: String
+}
