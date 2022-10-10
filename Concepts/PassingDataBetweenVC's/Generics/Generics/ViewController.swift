f1b1ed20810af0e01835db35aba04fc2b608c1d2
Sebastian <>
2022-10-10 14:49:15 -0500

    Set up generics project

---
+++ Concepts/PassingDataBetweenVC's/Generics/Generics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Generics
+//
+//  Created by Sebastian on 10/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
