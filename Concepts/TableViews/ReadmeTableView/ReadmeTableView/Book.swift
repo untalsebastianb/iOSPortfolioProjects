3ca7cc64c29ab62ef5a4e745f4bfe783931bc4f8
Sebastian <>
2022-10-18 09:51:48 -0500

    Create a table view with static cells and add a done button for the keyboard to hide

--- Concepts/TableViews/ReadmeTableView/ReadmeTableView/Book.swift
+++ Concepts/TableViews/ReadmeTableView/ReadmeTableView/Book.swift
@@ -11,6 +11,7 @@ import UIKit
 struct Book {
     let title: String
     let author: String
+    var review: String?
     var image: UIImage {
         Library.loadImage(forBook: self) ??
         LibrarySymbol.letterSquare(letter: title.first).image
--- Concepts/TableViews/ReadmeTableView/ReadmeTableView/DetailViewController.swift
+++ Concepts/TableViews/ReadmeTableView/ReadmeTableView/DetailViewController.swift
@@ -8,13 +8,14 @@
 import Foundation
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UITableViewController {
     
     let book: Book
     
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var authorLabel: UILabel!
     @IBOutlet var imageView: UIImageView!
+    @IBOutlet var reviewTextView: UITextView!
     
     @IBAction func updateImage() {
         let imagePicker = UIImagePickerController()
@@ -27,10 +28,16 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         imageView.image = book.image
         imageView.layer.cornerRadius = 16
         titleLabel.text = book.title
         authorLabel.text = book.author
+        if let review = book.review {
+            reviewTextView.text = review
+        }
+        
+        reviewTextView.addDoneButton()
     }
     
     required init?(coder: NSCoder) {
@@ -49,5 +56,26 @@ extension DetailViewController: UIImagePickerControllerDelegate, UINavigationCon
         imageView.image = selectedImage
         Library.saveImage(selectedImage, forBook: book)
         dismiss(animated: true)
+    }
+}
+
+extension DetailViewController: UITextViewDelegate {
+    func textViewDidEndEditing(_ textView: UITextView) {
+        textView.resignFirstResponder()
+    }
+}
+
+extension UITextView {
+    func addDoneButton() {
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(self.resignFirstResponder))
+        
+        toolbar.items = [flexSpace, doneButton]
+        self.inputAccessoryView = toolbar
+        
+        
+        
     }
 }
