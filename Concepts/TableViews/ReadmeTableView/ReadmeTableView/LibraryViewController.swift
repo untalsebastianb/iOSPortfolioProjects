4845a787818c1be741952ae88a07f0a7e684a93d
Sebastian <>
2022-10-22 10:35:06 -0500

    Add 2 sections and 1 header to the section 0

--- Concepts/TableViews/ReadmeTableView/ReadmeTableView/LibraryViewController.swift
+++ Concepts/TableViews/ReadmeTableView/ReadmeTableView/LibraryViewController.swift
@@ -7,17 +7,24 @@
 
 import UIKit
 
+
+class LibraryHeaderView: UITableViewHeaderFooterView {
+    static let reuseIdentifier = "\(LibraryHeaderView.self)"
+    @IBOutlet var titleLabel: UILabel!
+}
+
 class LibraryViewController: UITableViewController {
 
     @IBSegueAction func showDetailView(_ coder: NSCoder) -> DetailViewController? {
         guard let indexPath = tableView.indexPathForSelectedRow else { fatalError() }
-        let book = Library.books[indexPath.row - 1]
+        let book = Library.books[indexPath.row]
         return DetailViewController(coder: coder, book: book)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.register(UINib(nibName: "\(LibraryHeaderView.self)", bundle: nil), forHeaderFooterViewReuseIdentifier: LibraryHeaderView.reuseIdentifier)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -25,10 +32,28 @@ class LibraryViewController: UITableViewController {
         tableView.reloadData()
     }
     
+//    MARK: - Delegate
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        2
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        if section == 0 { return nil}
+        guard let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: LibraryHeaderView.reuseIdentifier) as? LibraryHeaderView else { return nil }
+        
+        headerView.titleLabel.text = "Read Me!"
+        return headerView
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return section != 0 ? 60 : 0
+    }
+    
     
     // MARK: - DataSource
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return Library.books.count + 1
+        return section == 0 ? 1 : Library.books.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -40,7 +65,7 @@ class LibraryViewController: UITableViewController {
         
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "\(BookCell.self)", for: indexPath) as? BookCell else { fatalError("Could not create the bookCell")}
         
-        let book = Library.books[indexPath.row - 1]
+        let book = Library.books[indexPath.row]
         cell.title.text = book.title
         cell.authorLabel.text = book.author
         cell.bookThumbnail.image = book.image
