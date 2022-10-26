3a1ca5f3b49f72e054224250ebf65481d2bc52fe
Sebastian <>
2022-10-26 06:53:40 -0500

    [YOUTUBEAPP] Setting up the project launchscreen and create the icons, colors.

---
+++ YoutubeClone-UIKit/YoutubeCloneUIKit/YoutubeCloneUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YoutubeCloneUIKit
+//
+//  Created by Sebastian on 26/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
