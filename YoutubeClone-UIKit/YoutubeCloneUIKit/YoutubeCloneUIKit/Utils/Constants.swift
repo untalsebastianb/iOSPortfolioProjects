8db1847f1dcca40d2f058b813137c10258f1fa2a
Sebastian <>
2022-11-08 07:15:51 -0500

    [YOUTUBEAPP] Create entirely a Generic service layer

---
+++ YoutubeClone-UIKit/YoutubeCloneUIKit/YoutubeCloneUIKit/Utils/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  YoutubeCloneUIKit
+//
+//  Created by Sebastian on 7/11/22.
+//
+
+import Foundation
+
+struct Constants {
+    static let apiKey = "AIzaSyB36MDztDNJpcAzKjZZx1BLzHmYjroiLaQ"
+
+}
