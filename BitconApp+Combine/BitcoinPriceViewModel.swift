93ea42e75c99413ead2a439181cc1710e869b27f
Sebastian <>
2023-02-13 11:49:35 -0500

    Create subscription on the view model to the API service call

--- BitconApp+Combine/BitcoinPriceViewModel.swift
+++ BitconApp+Combine/BitcoinPriceViewModel.swift
@@ -14,7 +14,27 @@ class BitcoinPriceViewModel: ObservableObject {
         PriceDetails(currency: $0)
     }
     
+    private var subscription: AnyCancellable?
+    
     public func onAppear() {
         // get data from the API
+        subscription = CoindeskAPIService.shared.fetchBitcoinPrice()
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .finished:
+                    print("Success")
+                case .failure(let error):
+                    print("\(error.localizedDescription)")
+                }
+            }, receiveValue: { [weak self] value in
+                guard let self = self else { return }
+                self.lastUpdated = value.time.updated
+                self.priceDetails = [
+                    PriceDetails(currency: .usd, rate: value.bpi.USD.rate),
+                    PriceDetails(currency: .eur, rate: value.bpi.EUR.rate),
+                    PriceDetails(currency: .gbp, rate: value.bpi.GBP.rate),
+                ]
+            })
+        
     }
 }
