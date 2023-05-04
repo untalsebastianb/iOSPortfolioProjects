69d5114e09ffd201091b984fee6c2b6955c82057
JuanchoBueno <JuanSebastian.Bueno@dcsg.com>
2023-05-04 07:16:15 -0500

    [Uber] - Create folder tree

---
+++ Uber/Uber/App/UberApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberApp.swift
+//  Uber
+//
+//  Created by Juan Sebastian Bueno on 4/05/23.
+//
+
+import SwiftUI
+
+@main
+struct UberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ Uber/Uber/Core/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Uber
+//
+//  Created by Juan Sebastian Bueno on 4/05/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
