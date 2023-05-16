9fd8a264e9d7e7b4468209b3c637b3017ee41e56
JuanchoBueno <JuanSebastian.Bueno@dcsg.com>
2023-05-16 07:31:11 -0500

    [FigmaToCode] - Create folder tree

---
+++ FigmaToCode/FigmaToCode/FigmaToCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FigmaToCodeApp.swift
+//  FigmaToCode
+//
+//  Created by Juan Sebastian Bueno on 16/05/23.
+//
+
+import SwiftUI
+
+@main
+struct FigmaToCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
