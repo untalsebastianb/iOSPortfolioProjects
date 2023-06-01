86ed5c4a64136eb7dd209a4c42b2f81b771060fb
JuanchoBueno <JuanSebastian.Bueno@dcsg.com>
2023-06-01 10:30:58 -0500

    [ShoppingApp] Create project

---
+++ ShoppingApp/ShoppingApp/ShoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingAppApp.swift
+//  ShoppingApp
+//
+//  Created by Juan Sebastian Bueno on 1/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
