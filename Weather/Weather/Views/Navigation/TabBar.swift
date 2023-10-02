d5852541ae00dde785a34711ed75d467499723ca
JuanchoBueno <JuanSebastian.Bueno@dcsg.com>
2023-10-02 11:47:54 -0500

    [Weather App] - Create custom shape for tabBar

--- Weather/Weather/Views/Navigation/TabBar.swift
+++ Weather/Weather/Views/Navigation/TabBar.swift
@@ -11,6 +11,16 @@ struct TabBar: View {
     var action: () -> Void
     var body: some View {
         ZStack {
+//            MARK: Arc Shape
+            Arc()
+                .fill(Color.tabBarBackground)
+                .frame(height: 88)
+                .overlay {
+                    Arc()
+                        .stroke(Color.tabBarBorder, lineWidth: 0.5)
+                }
+            
+//            MARK: Tab Items
             HStack {
                 // MARK: Expand Button
                 Button { 
