8296ebbe07f23ddaa3ed181ab9e08c1c51b5bc94
JuanchoBueno <JuanSebastian.Bueno@dcsg.com>
2023-10-16 14:00:32 -0500

    [CryptoTracker] - Create extensions for string management

--- SwiftUICrypto/SwiftUICrypto/Extensions/Double.swift
+++ SwiftUICrypto/SwiftUICrypto/Extensions/Double.swift
@@ -8,6 +8,33 @@
 import Foundation
 
 extension Double {
+    
+    /// converts a double into a Currency with 2 decimal places
+    /// ```
+    /// Convert 123.56 to $1,234.56
+
+    /// ``
+    private var currencyFormatter2: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+//        formatter.locale = .current
+//        formatter.currencyCode = "usd"
+//        formatter.currencySymbol = "$"
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }
+    
+    /// converts a double into a Currency as String with 2 decimal places
+    /// ```
+    /// Convert 123.56 to "$1,234.56"
+    /// ```
+    func asCurrencyWith2Decimals() -> String {
+        let number = NSNumber(value: self)
+        return currencyFormatter2.string(from: number) ?? "$0.00"
+    }
+    
     /// converts a double into a Currency with 2-6 decimal places
     /// ```
     /// Convert 123.56 to $1,234.56
@@ -31,9 +58,25 @@ extension Double {
     /// Convert 123.56 to "$1,234.56"
     /// Convert 12.3456 to "$12.3456"
     /// Convert 0.123456 to "$0.123456"
-    /// ``
+    /// ```
     func asCurrencyWith6Decimals() -> String {
         let number = NSNumber(value: self)
         return currencyFormatter6.string(from: number) ?? "$0.00"
+    }
+    
+    /// converts a double into a String representation 
+    /// ```
+    /// Convert 1.2345 to "1.23"
+    /// ```
+    func asNumberString() -> String {
+        return String(format: "%.2f", self)
+    }
+    
+    /// converts a double into a String representation with percent symbol
+    /// ```
+    /// Convert 1.2345 to "1.23%"
+    /// ```
+    func asPercentString() -> String {
+        return asNumberString() + "%"
     }
 }
