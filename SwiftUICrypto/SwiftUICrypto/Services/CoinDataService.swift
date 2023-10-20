dd959d122cb9ae7cc426a2136aa19409ad8df7f2
JuanchoBueno <JuanSebastian.Bueno@dcsg.com>
2023-10-20 16:06:16 -0500

    [CryptoTracker] Create Networking manager

--- SwiftUICrypto/SwiftUICrypto/Services/CoinDataService.swift
+++ SwiftUICrypto/SwiftUICrypto/Services/CoinDataService.swift
@@ -20,29 +20,11 @@ class CoinDataService {
     private func getCoins()  {
         guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else { return }
         
-        coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
-            .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap { (output) in 
-                guard let response = output.response as? HTTPURLResponse,
-                      response.statusCode >= 200 && response.statusCode < 300 else {
-                    throw URLError(.badServerResponse)
-                }
-                return output.data
-            }
-            .receive(on: DispatchQueue.main)
+        coinSubscription = NetworkingManager.download(url: url)
             .decode(type: [CoinModel].self, decoder: JSONDecoder())
-            .sink { completion in
-                switch completion {
-                case .finished: 
-                    break
-                case .failure(let error):
-                    print(String(describing: error))
-                }
-                
-            } receiveValue: { [weak self] returnedCoins in
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedCoins in
                 self?.allCoins = returnedCoins
-                self?.coinSubscription?.cancel()
-            }
+                self?.coinSubscription?.cancel()})
 
     }
 }
