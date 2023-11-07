445db6791556724799613dd85d1f9d781fc821b7
JuanchoBueno <JuanSebastian.Bueno@dcsg.com>
2023-11-07 10:06:05 -0500

    [CryptoTracker] - Create Model and service to download market data to view

--- SwiftUICrypto/SwiftUICrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftUICrypto/SwiftUICrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -20,7 +20,9 @@ class HomeViewModel: ObservableObject {
     @Published var searchText: String = ""
     private var cancellables = Set<AnyCancellable>()
     
-    private let dataService = CoinDataService()
+    private let coinDataService = CoinDataService()
+    private let marketDataService = MarketDataService()
+    
     
     init() {
         addSubscribers()
@@ -30,12 +32,40 @@ class HomeViewModel: ObservableObject {
         // updates all coins        
         $searchText
             .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
-            .combineLatest(dataService.$allCoins)
+            .combineLatest(coinDataService.$allCoins)
             .map(filterCoins)
             .sink { [weak self] returnedCoins in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+        
+        // updates marketData        
+        marketDataService.$marketData
+            .map(mapGlobalMarketData)
+            .sink { [weak self] stats in
+                self?.statistics = stats
+            }
+            .store(in: &cancellables)
+        
+    }
+    
+    private func mapGlobalMarketData(marketDataModel: MarketDataModel?) -> [StatisticsModel] {
+        var stats: [StatisticsModel] = []
+        guard let data = marketDataModel else {
+            return stats
+        }
+        let marketCap = StatisticsModel(title: "Market Cap", value: data.marketCap, percentageChange: data.marketCapChangePercentage24HUsd)
+        let volume = StatisticsModel(title: "24H Volume", value: data.volume)
+        let btcDominance = StatisticsModel(title: "BTC Dominance", value: data.btcDominance)
+        let portfolio = StatisticsModel(title: "Portfolio Value", value: "", percentageChange: 0)
+        
+        stats.append(contentsOf: [
+            marketCap,
+            volume,
+            btcDominance,
+            portfolio
+        ])
+        return stats
     }
     
     private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
---
+++ SwiftUICrypto/SwiftUICrypto/Services/MarketDataService.swift
@@ -0,0 +1,30 @@
+//
+//  MarketDataService.swift
+//  SwiftUICrypto
+//
+//  Created by Juan Sebastian Bueno on 11/7/23.
+//
+
+import Foundation
+import Combine
+
+class MarketDataService {
+    @Published var marketData: MarketDataModel? = nil
+    var marketDataSubscription: AnyCancellable?
+    
+    init() {
+        getData()
+    }
+    
+    
+    private func getData()  {
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/global") else { return }
+        
+        marketDataSubscription = NetworkingManager.download(url: url)
+            .decode(type: GlobalData.self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedGlobalData in
+                self?.marketData = returnedGlobalData.data
+                self?.marketDataSubscription?.cancel()})
+
+    }
+}
