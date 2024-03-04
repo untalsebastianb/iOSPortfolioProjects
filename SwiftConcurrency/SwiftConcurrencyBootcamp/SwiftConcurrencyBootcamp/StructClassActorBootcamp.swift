e0e42d7c5802fee77d382e8d1e34859afe63aa3d
untalsebastianb <jsebasti.ma@gmail.com>
2024-03-04 08:34:02 -0500

    [SwiftConcurrency] structs bootcamp mutating

--- SwiftConcurrency/SwiftConcurrencyBootcamp/SwiftConcurrencyBootcamp/StructClassActorBootcamp.swift
+++ SwiftConcurrency/SwiftConcurrencyBootcamp/SwiftConcurrencyBootcamp/StructClassActorBootcamp.swift
@@ -11,7 +11,7 @@ struct StructClassActorBootcamp: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
             .onAppear{
-                runTest()
+                structTest2()
             }
     }
 }
@@ -77,4 +77,58 @@ class myClass {
 
 #Preview {
     StructClassActorBootcamp()
+}
+
+// Inmutable struct
+// everything is going to be a let inside the struct
+struct customStruct {
+    let title: String
+    
+    func updateTitle(newTitle: String) -> customStruct
+    {
+        .init(title: newTitle)
+    }
+}
+
+struct MutatingStruct {
+    private(set) var title: String
+    
+    init(title: String) {
+        self.title = title
+    }
+    
+    // this is to filter the way the struct is mutating.
+    mutating func updateTitle(newTitle: String) {
+        title = newTitle
+    }
+}
+
+
+extension StructClassActorBootcamp {
+    private func structTest2() {
+        print("structTest2")
+        var struct1 = myStruct(title: "Title1")
+        print("struct1", struct1.title)
+        struct1.title = "title2"
+        print("struct1", struct1.title)
+        
+        printDivider()
+        var struct2 = customStruct(title: "title1")
+        print("struct2", struct2.title)
+        struct2 = customStruct(title: "title2")
+        print("struct2", struct1.title)
+        
+        printDivider()
+        var struct3 = customStruct(title: "title1")
+        print("struct3", struct3.title)
+        struct3 = struct3.updateTitle(newTitle: "title2")
+        print("struct3", struct3.title)
+        
+        
+        var struct4 = MutatingStruct(title: "title1")
+        print("struct4", struct4.title)
+        struct4.updateTitle(newTitle: "title2")
+        print("struct4", struct4.title)
+        
+    }
 }
