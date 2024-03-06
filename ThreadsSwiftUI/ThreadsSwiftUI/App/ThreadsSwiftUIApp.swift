982728f349fe3144616296f808570ae36de60808
untalsebastianb <jsebasti.ma@gmail.com>
2024-03-06 11:29:26 -0500

    [ThreadsApp] - Create project and folder structure

---
+++ ThreadsSwiftUI/ThreadsSwiftUI/App/ThreadsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsSwiftUIApp.swift
+//  ThreadsSwiftUI
+//
+//  Created by Juan Sebastian Bueno on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
