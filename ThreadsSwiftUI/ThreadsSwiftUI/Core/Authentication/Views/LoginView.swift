d3ac11161ee4cae4918e85d552c80eafb418b27f
untalsebastianb <jsebasti.ma@gmail.com>
2024-03-08 09:03:31 -0500

    [ThreadsApp] - Create login view

--- ThreadsSwiftUI/ThreadsSwiftUI/Core/Authentication/Views/LoginView.swift
+++ ThreadsSwiftUI/ThreadsSwiftUI/Core/Authentication/Views/LoginView.swift
@@ -8,8 +8,74 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State private(set) var email = ""
+    @State private(set) var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Spacer()
+                Image("ThreadsLogo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 120, height: 120)
+                    .padding()
+                
+                VStack {
+                    TextField("Enter your Email", text: $email)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                    SecureField("Enter your password", text: $password)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                } // Vstack
+                
+                NavigationLink {
+                    Text("Forgot password")
+                } label: {
+                    Text("Forgot Password")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.vertical)
+                        .padding(.trailing , 28)
+                        .foregroundColor(.black)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                }// Nv Link
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Text("Login")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.white)
+                        .frame(width: 352, height: 44)
+                        .background(.black)
+                        .cornerRadius(8)
+                })
+                
+                Spacer()
+                Divider()
+                NavigationLink { 
+                    Text("RegistrationView")
+                } label: { 
+                    HStack(spacing: 3) {
+                        Text("Don't have an account?")
+                        Text("Sign Up")
+                            .fontWeight(.semibold)
+                    }
+                    .foregroundStyle(.black)
+                    .font(.footnote)
+                }
+                .padding(.vertical, 16)
+
+            } // Vstack
+        }
     }
 }
 
