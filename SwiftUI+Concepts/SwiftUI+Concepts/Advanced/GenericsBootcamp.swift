641838a94052c827e7ac697103e8c9e4ee21922c
untalsebastianb <jsebasti.ma@gmail.com>
2024-03-30 10:54:19 -0500

    [Swift+Concepts] - Generics

--- SwiftUI+Concepts/SwiftUI+Concepts/Advanced/GenericsBootcamp.swift
+++ SwiftUI+Concepts/SwiftUI+Concepts/Advanced/GenericsBootcamp.swift
@@ -7,15 +7,54 @@
 
 import SwiftUI
 
+struct StringModel {
+    let info: String?
+    func removeInfo() -> StringModel {
+        return StringModel(info: nil)
+    }
+}
+
+struct boolModel {
+    let info: Bool?
+    func removeInfo() -> boolModel {
+        return boolModel(info: nil)
+    }
+}
+
+struct GenericModel<T> {
+    let info: T?
+    func removeInfo() -> GenericModel {
+        return GenericModel(info: nil)
+    }
+}
+
+struct GenericView<CustomType: View>: View {
+    let content: CustomType
+    var body: some View {
+        VStack {
+            content
+            Text("Hi G View") 
+        }
+    }
+}
+
 class GenericsViewModel: ObservableObject {
+    
+    @Published var stringModel = StringModel(info: "Hello World!")
+    @Published var booleanModel = boolModel(info: true)
+    @Published var genericStringModel = GenericModel(info: "Hello Generics")
+    @Published var genericBoolModel = GenericModel(info: true)
     @Published var dataArray: [String] = []
     
     init() {
         dataArray = ["one", "two", "three"]
     }
     
-    func removeDataFromDataArray() {
-        dataArray.removeAll()
+    func removeData() {
+        stringModel = stringModel.removeInfo()
+        booleanModel = booleanModel.removeInfo()
+        genericStringModel = genericStringModel.removeInfo()
+        genericBoolModel = genericBoolModel.removeInfo()
     }
 }
 
@@ -25,12 +64,17 @@ struct GenericsBootcamp: View {
     
     var body: some View {
         VStack {
-            ForEach(vm.dataArray, id: \.self) { item in
-                Text(item)
-                    .onTapGesture {
-                        vm.removeDataFromDataArray()
-                    }
-            }
+            
+            GenericView(content: Text("Hii gg view"))
+            
+            Text(vm.stringModel.info ?? "no data")
+            Text(vm.booleanModel.info?.description ?? "no data")
+            Text(vm.genericBoolModel.info?.description ?? "no data")
+            Text(vm.genericStringModel.info?.description ?? "no data")
+            
+        }
+        .onTapGesture {
+            vm.removeData()
         }
     }
 }
