f1316f9ed9fb8f862bbfd90826f5772352166454
untalsebastianb <jsebasti.ma@gmail.com>
2024-04-02 20:57:44 -0500

    [ThreadsApp] -  Create layout for Feed and Explore view

--- ThreadsSwiftUI/ThreadsSwiftUI/Core/Feed/Views/FeedView.swift
+++ ThreadsSwiftUI/ThreadsSwiftUI/Core/Feed/Views/FeedView.swift
@@ -9,10 +9,33 @@ import SwiftUI
 
 struct FeedView: View {
     var body: some View {
-        Text("Hello feed")
+        NavigationStack {
+            ScrollView(showsIndicators: false) {
+                LazyVStack {
+                    ForEach(0 ... 10, id: \.self) { thread in 
+                        ThreadView()
+                    }
+                }
+            }
+            .refreshable {
+                print("DEBUG: refresh threads")
+            }
+            .navigationTitle("Threads")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        .toolbar(content: {
+            ToolbarItem(placement: .topBarTrailing) { 
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Image(systemName: "arrow.counterclockwise")
+                        .foregroundStyle(.black)
+                })
+            }
+        })
     }
 }
 
 #Preview {
-    FeedView()
+    NavigationStack {
+        FeedView()
+    }
 }
